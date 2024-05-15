b9d162b58d16bde05553d050fc8f6d54b0dd5e3a
MrBisquit <william@wtdawson.info>
2024-05-15 21:37:03 +0100

    Added a new button

--- EasyMarkdownDocs/MainWindow.xaml.cs
+++ EasyMarkdownDocs/MainWindow.xaml.cs
@@ -58,7 +58,26 @@ namespace EasyMarkdownDocs
 
         private void MMFOpenFolder_Click(object sender, RoutedEventArgs e)
         {
+            VistaFolderBrowserDialog fbd = new VistaFolderBrowserDialog();
+            if((bool)fbd.ShowDialog())
+            {
+                TaskDialog init = new TaskDialog()
+                {
+                    WindowTitle = "EasyMarkdownDocs",
+                    Content = "Would you like to initialise documentation for this project? The structure will gain these (If it does not already have them):\n" +
+                    "docs/README.md\n" +
+                    "README.md\n\n" +
+                    "It will also automatically gain an EasyMarkdownDocs.json file for containing information to be able to generate the markdown docs for you."
+                };
 
+                TaskDialogButton yes = new TaskDialogButton() { ButtonType = ButtonType.Yes };
+                TaskDialogButton no = new TaskDialogButton() { ButtonType = ButtonType.No };
+
+                init.Buttons.Add(yes);
+                init.Buttons.Add(no);
+
+                TaskDialogButton initResult = init.ShowDialog();
+            }
         }
     }
 }
